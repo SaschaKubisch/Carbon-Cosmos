25573366b72acf5f06c58662068ae5d261f5326c
Sascha <sascha.fh.kubisch@gmail.com>
2023-05-16 14:49:14 +0200

    Added transactionRoutes

---
+++ server/api/routes/transactionRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const transactionController = require('../controllers/transactionController');
+
+// Middleware to check if the user is authenticated
+// You can implement it according to your authentication strategy
+const isAuthenticated = require('../middleware/auth');
+
+// Route to calculate the carbon offset
+router.post('/calculate-offset', isAuthenticated, transactionController.calculateOffset);
+
+// Route to select carbon credits
+router.post('/select-credit', isAuthenticated, transactionController.selectCredit);
+
+// Route to create a new transaction and generate the receipt
+router.post('/create', isAuthenticated, transactionController.createTransaction);
+
+// Route to get a transaction by its ID
+router.get('/:transactionId', isAuthenticated, transactionController.getTransaction);
+
+module.exports = router;
