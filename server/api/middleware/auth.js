c5e9217eccabf960674978ad80a30d7b93bc355b
Sascha <sascha.fh.kubisch@gmail.com>
2023-05-17 17:05:37 +0200

    Added authentication and error handling middleware

---
+++ server/api/middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware for checking JWT
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, 'your_secret_key', (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+module.exports = authenticateJWT;
