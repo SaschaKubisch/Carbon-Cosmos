a1d2f9f7f6b89549135808a6a985735c5980eff9
Sascha <sascha.fh.kubisch@gmail.com>
2023-05-22 19:35:34 +0200

    Edited bridge contract interaction for cosmwasm

--- server/blockchain/bridgeContract.js
+++ server/blockchain/bridgeContract.js
@@ -1,38 +1,73 @@
-const ethers = require('ethers');
+const { SigningCosmWasmClient } = require("@cosmjs/cosmwasm-launchpad");
 
 class BridgeContract {
-  constructor(providerUrl, privateKey) {
-    this.provider = new ethers.providers.JsonRpcProvider(providerUrl);
-    this.wallet = new ethers.Wallet(privateKey, this.provider);
-    this.bridgeContract = new ethers.Contract('BridgeContractAddress', 'ABI', this.wallet);
+  constructor(url, chainId, mnemonic) {
+    this.client = undefined;
+    this.url = url;
+    this.chainId = chainId;
+    this.mnemonic = mnemonic;
   }
 
-  async offsetTransaction(receiverAddress, amount, receiverNetwork, offsetType, offsetAmount, carbonCreditType) {
-    let tx = await this.bridgeContract.offsetTransaction(receiverAddress, amount, receiverNetwork, offsetType, offsetAmount, carbonCreditType);
-    let receipt = await tx.wait();
-    return receipt;
+  async connect() {
+    this.client = await SigningCosmWasmClient.connectWithSigner(this.url, this.mnemonic);
   }
 
-  async burnCarbonCredits(amount, carbonCreditType) {
-    let tx = await this.bridgeContract.burnCarbonCredits(amount, carbonCreditType);
-    let receipt = await tx.wait();
-    return receipt;
+  async offsetTransaction(contractAddress, receiverAddress, amount, receiverNetwork, offsetType, offsetAmount, carbonCreditType) {
+    const msg = {
+      offset_transaction: {
+        receiver_address: receiverAddress,
+        amount: amount,
+        receiver_network: receiverNetwork,
+        offset_type: offsetType,
+        offset_amount: offsetAmount,
+        carbon_credit_type: carbonCreditType,
+      },
+    };
+
+    const result = await this.client.execute(contractAddress, msg);
+    return result;
   }
 
-  async addCarbonCreditsToPool(amount, carbonCreditType) {
-    let tx = await this.bridgeContract.addCarbonCreditsToPool(amount, carbonCreditType);
-    let receipt = await tx.wait();
-    return receipt;
+  async burnCarbonCredits(contractAddress, amount, carbonCreditType) {
+    const msg = {
+      burn_carbon_credits: {
+        amount: amount,
+        carbon_credit_type: carbonCreditType,
+      },
+    };
+
+    const result = await this.client.execute(contractAddress, msg);
+    return result;
   }
 
-  async getCarbonCreditsPool() {
-    let pool = await this.bridgeContract.carbonCreditsPool();
-    return pool;
+  async addCarbonCreditsToPool(contractAddress, amount, carbonCreditType) {
+    const msg = {
+      add_carbon_credits_to_pool: {
+        amount: amount,
+        carbon_credit_type: carbonCreditType,
+      },
+    };
+
+    const result = await this.client.execute(contractAddress, msg);
+    return result;
   }
 
-  async getOffsetTransaction(transactionId) {
-    let transaction = await this.bridgeContract.offsetTransaction(transactionId);
-    return transaction;
+  async getCarbonCreditsPool(contractAddress) {
+    const msg = {
+      carbon_credits_pool: {},
+    };
+
+    const result = await this.client.queryContractSmart(contractAddress, msg);
+    return result;
+  }
+
+  async getOffsetTransaction(contractAddress, transactionId) {
+    const msg = {
+      offset_transaction: { transaction_id: transactionId },
+    };
+
+    const result = await this.client.queryContractSmart(contractAddress, msg);
+    return result;
   }
 }
 
