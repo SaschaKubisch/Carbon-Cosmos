728fd522ffbe33c1c7d20c89ea85f4489a61c55d
Sascha <sascha.fh.kubisch@gmail.com>
2023-05-23 16:10:44 +0200

    Added database connection

---
+++ server/db/index.js
@@ -0,0 +1,22 @@
+const { Sequelize } = require('sequelize');
+
+// Option 1: Passing a connection URI
+const sequelize = new Sequelize('postgres://user:pass@localhost:5432/dbname');
+
+// Option 2: Passing parameters separately (other dialects)
+// const sequelize = new Sequelize('database', 'username', 'password', {
+//   host: 'localhost',
+//   dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+// });
+
+// testing the connection
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
+
+module.exports = sequelize;
